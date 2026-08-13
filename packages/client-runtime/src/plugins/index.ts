b0e26ae3d1e05f811f1afab3ea04301dde8c0160
gmackie <graham.mackie@gmail.com>
2026-08-12 21:28:57 -0400

    feat(plugins): ship standalone plugin runtime

---
+++ packages/client-runtime/src/plugins/index.ts
@@ -0,0 +1,7 @@
+export * from "./registry.ts";
+export * from "./workSource.ts";
+export * from "./host.ts";
+export * from "./compatibility.ts";
+export * from "./mobile.ts";
+export * from "./remote.ts";
+export * from "./navigation.ts";
