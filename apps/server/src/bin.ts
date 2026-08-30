7b1d215e3249f90c6b867681688141270fe8bee9
Graham Mackie <graham.mackie@gmail.com>
2026-08-29 20:07:16 -0400

    feat(server): add backup-first activity retention

--- apps/server/src/bin.ts
+++ apps/server/src/bin.ts
@@ -12,6 +12,7 @@ import { connectCommand } from "./cli/connect.ts";
 import { pairCommand } from "./cli/pair.ts";
 import { hasCloudPublicConfig } from "./cloud/publicConfig.ts";
 import { sharedServerCommandFlags } from "./cli/config.ts";
+import { maintenanceCommand } from "./cli/maintenance.ts";
 import { isEntrypoint } from "./entrypoint.ts";
 import { projectCommand } from "./cli/project.ts";
 import { runServerCommand, serveCommand, startCommand } from "./cli/server.ts";
@@ -56,6 +57,7 @@ export const makeCli = ({ cloudEnabled = hasCloudPublicConfig } = {}) =>
       pairCommand,
       authCommand,
       projectCommand,
+      maintenanceCommand,
       serviceCommand,
       servicePreflightCommand,
       themeCommand,
