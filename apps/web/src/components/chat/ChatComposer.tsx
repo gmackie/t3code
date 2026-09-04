98dd7a44fc7aa3f18342577a7476287f8e260a36
gmackie <graham.mackie@gmail.com>
2026-09-03 20:13:40 -0400

    Revert "feat(chat): switch providers in existing threads"
    This reverts commit 0bc8bbe788ef64c9d185d355062d4fe4a15e8f8a.

--- apps/web/src/components/chat/ChatComposer.tsx
+++ apps/web/src/components/chat/ChatComposer.tsx
@@ -1315,6 +1315,8 @@ export interface ChatComposerProps {
   ) => void;
 
   onProviderModelSelect: (instanceId: ProviderInstanceId, model: string) => void;
+  onOpenProviderSetup: (instanceId: ProviderInstanceId) => void;
+  getModelDisabledReason: (instanceId: ProviderInstanceId, model: string) => string | null;
   toggleInteractionMode: () => void;
   handleRuntimeModeChange: (mode: RuntimeMode) => void;
   handleInteractionModeChange: (mode: ProviderInteractionMode) => void;
@@ -1407,6 +1409,8 @@ export const ChatComposer = memo(function ChatComposer(props: ChatComposerProps)
     onPreviousActivePendingUserInputQuestion,
     onChangeActivePendingUserInputCustomAnswer,
     onProviderModelSelect,
+    onOpenProviderSetup,
+    getModelDisabledReason,
     toggleInteractionMode,
     handleRuntimeModeChange,
     handleInteractionModeChange,
@@ -5593,9 +5597,9 @@ export const ChatComposer = memo(function ChatComposer(props: ChatComposerProps)
                       No provider available
                     </Button>
                   ) : (
-                    <ProviderModelPicker
-                      environmentId={environmentId}
-                      compact={isComposerFooterCompact}
+                  <ProviderModelPicker
+                    environmentId={environmentId}
+                    compact={isComposerFooterCompact}
                       activeInstanceId={selectedInstanceId}
                       model={selectedModelForPickerWithCustomFallback}
                       lockedProvider={lockedProvider}
@@ -5615,6 +5619,7 @@ export const ChatComposer = memo(function ChatComposer(props: ChatComposerProps)
                       onOpenChange={(open) => {
                         setIsComposerModelPickerOpen(open);
                       }}
+                      getModelDisabledReason={getModelDisabledReason}
                       onInstanceModelChange={onProviderModelSelect}
                     />
                   )}
